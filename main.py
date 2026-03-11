cf72004bce0501c153bf457eda79919ac3618495
Maclins-ACMK <adolphusmk@gmail.com>
2026-03-11 10:02:15 +0100

    Enable CORS for frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine
 from models import Base
 
@@ -6,6 +7,18 @@ from routers.auth_routes import router as auth_router
 from routers.link_routes import router as link_router
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
