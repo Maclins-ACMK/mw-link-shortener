da332f57ee261f6a36136703b5cc2576ae93f87d
Maclins-ACMK <adolphusmk@gmail.com>
2026-03-11 11:13:38 +0100

    enable cors

--- frontend/app.js
+++ frontend/app.js
@@ -54,8 +54,9 @@ body:JSON.stringify({original_url:url})
 })
 
 let data = await res.json()
+console.log(data)
 
-document.getElementById("result").innerText =
-API + "/" + data.short_code
+document.getElementById("result").innerHTML =
+`<a href="${data.short_url}" target="_blank">${data.short_url}</a>`
 
 }
